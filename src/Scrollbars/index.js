8c7afcefc88e67ce8a339472be473bdc3f402bcd
Luthfi GearIntellix <luthfigearintellix@gmail.com>
2022-04-09 17:04:05 +0700

    fix: offset content size

--- src/Scrollbars/index.js
+++ src/Scrollbars/index.js
@@ -529,6 +529,8 @@ export default class Scrollbars extends Component {
             // Hide scrollbars by setting a negative margin
             marginRight: scrollbarWidth ? -scrollbarWidth : 0,
             marginBottom: scrollbarWidth ? -scrollbarWidth : 0,
+            paddingRight: scrollbarWidth ? -scrollbarWidth : 0,
+            paddingBottom: scrollbarWidth ? -scrollbarWidth : 0,
             ...(autoHeight && {
                 ...viewStyleAutoHeight,
                 // Add scrollbarWidth to autoHeight in order to compensate negative margins
